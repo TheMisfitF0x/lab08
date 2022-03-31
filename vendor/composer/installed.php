b168c1afa578a78dfadeb36b798b1af94937f57f
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2022-03-31 12:49:25 -0400

    composer wizardry
    What even is composition

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '4840e30298ebe55fbb9436c82d9c98e89db1180d',
+        'name' => 'vendor_name/lab08',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'vendor_name/lab08' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '4840e30298ebe55fbb9436c82d9c98e89db1180d',
+            'dev_requirement' => false,
+        ),
+    ),
+);
