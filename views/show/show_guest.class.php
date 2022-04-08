e7badf1c142f0b9c7a7ca1b34842adf99791a28f
Evminor <98989012+Evminor@users.noreply.github.com>
2022-04-08 18:53:48 -0400

    Finished showed guess
    I think I got it right

--- views/show/show_guest.class.php
+++ views/show/show_guest.class.php
@@ -2,5 +2,20 @@
 
 class ShowGuest
 {
+    public function display($guest)
+    {
+        ?>
 
-}
+        <h2>Guest details</h2>
+
+        <ul>
+            <li>Firstname: <?=$guest->getFirstName() ?> </li>
+            <li>Lastname: <?=$guest->getLastName() ?></li>
+            <li>Birthdate: <?=$guest->getBirthDate() ?></li>
+            <li>Email:  <?=$guest->getEmail() ?> </li>
+        </ul>
+
+
+<?php
+    }
+}?>
--- views/sign/sign_guest.class.php
+++ views/sign/sign_guest.class.php
@@ -2,5 +2,7 @@
 
 class SignGuest
 {
-
-}
+    public function display(){
+        ?>
+<?php    }
+}?>
