50e884a3c1fec96e72d58c86c99c3838d2686fd1
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2022-04-08 19:01:35 -0400

    Update index.class.php

--- views/index/index.class.php
+++ views/index/index.class.php
@@ -13,16 +13,15 @@ class Index
         <div id="main-header">Edit Movie Details</div>
 
         <!-- display movie details in a form -->
-        <form class="new-media"  action='<?= BASE_URL . "/movie/update/" . $id ?>' method="post" style="border: 1px solid #bbb; margin-top: 10px; padding: 10px;">
-            <input type="hidden" name="id" value="<?= $id ?>">
-            <p><strong>Title</strong><br>
+        <form class="new-media"  action='../../index.php?action=sign' method="post" style="border: 1px solid #bbb; margin-top: 10px; padding: 10px;">
+            <p><strong>Name</strong><br>
                 <input name="name" type="text" size="100" value="" required autofocus></p>
             <p><strong>Birth Date</strong>: <input name="birth_date" type="date" value="" required=""></p>
             <p><strong>Email</strong><br>
                 <input name="email" type="email" size="40" value="" required=""></p>
 
-            <input type="submit" name="action" value="Update Movie">
-            <input type="button" value="Cancel" onclick='window.location.href = "<?= BASE_URL . "/movie/detail/" . $id ?>"'>
+            <input type="submit" name="action" value="Sign">
+
         </form>
         <?php
         //display page footer
