4c05c39127d1e2f67fdad575779ad781f85fe28d
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2022-04-08 20:04:51 -0400

    updated error

--- views/error/guest_error.class.php
+++ views/error/guest_error.class.php
@@ -2,5 +2,34 @@
 
 class GuestError
 {
+    public function display($message) {
 
+        ?>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <title> Error </title>
+        </head>
+    <body>
+        <div id="main-header">Error</div>
+        <hr>
+        <table style="width: 100%; border: none">
+            <tr>
+                <td style="vertical-align: middle; text-align: center; width:100px">
+                </td>
+                <td style="text-align: left; vertical-align: top;">
+                    <h3> Sorry, but an error has occurred.</h3>
+                    <div style="color: red">
+                        <?= urldecode($message) ?>
+                    </div>
+                    <br>
+                </td>
+            </tr>
+        </table>
+        <br><br><br><br><hr>
+        <a href="../../index.php?action=show">Back to guest list</a>
+    </body>
+        </html>
+        <?php
+    }
 }
--- views/index/index.class.php
+++ views/index/index.class.php
@@ -4,28 +4,38 @@ class Index
 {
 
     public function display() {
-        //display page header
-        parent::displayHeader("Welcome");
-
-
         ?>
+<!DOCTYPE html>
+        <html>
+            <head>
+                <title> Sign in </title>
 
-        <div id="main-header">Edit Movie Details</div>
+                <script>
+                    //create the JavaScript variable for the base url
+                    var base_url = "<?= BASE_URL ?>";
+                </script>
+            </head>
+            <body>
+                <div id="main-header">Sign In</div>
 
-        <!-- display movie details in a form -->
-        <form class="new-media"  action='../../index.php?action=sign' method="post" style="border: 1px solid #bbb; margin-top: 10px; padding: 10px;">
-            <p><strong>Name</strong><br>
-                <input name="name" type="text" size="100" value="" required autofocus></p>
-            <p><strong>Birth Date</strong>: <input name="birth_date" type="date" value="" required=""></p>
-            <p><strong>Email</strong><br>
-                <input name="email" type="email" size="40" value="" required=""></p>
 
-            <input type="submit" name="action" value="Sign">
+                <form class="new-media"  action='../../index.php?action=sign' method="post" style="border: 1px solid #bbb; margin-top: 10px; padding: 10px;">
+                    <p><strong>First Name</strong><br>
+                        <input name="first_name" type="text" size="100" value="" required autofocus></p>
+                    <p><strong>Last Name</strong><br>
+                        <input name="last_name" type="text" size="100" value="" required autofocus></p>
+                    <p><strong>Birth Date</strong>: <input name="birth_date" type="date" value="" required=""></p>
+                    <p><strong>Email</strong><br>
+                        <input name="email" type="email" size="40" value="" required=""></p>
 
-        </form>
+                    <input type="submit" name="action" value="Sign">
+
+                </form>
+            </body>
+        </html>
+
         <?php
-        //display page footer
-        parent::displayFooter();
+
 
     }
 }
